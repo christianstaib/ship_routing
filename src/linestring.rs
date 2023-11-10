17b3e02bf8c322ecda5b6d819a64195eb8c1e6b5
Christian Staib <staib.christian@hotmail.de>
2023-11-10 23:44:38 +0100

    added fix for dateline

--- src/linestring.rs
+++ src/linestring.rs
@@ -11,8 +11,8 @@ impl Linestring {
     pub fn new(points: Vec<Point>) -> Linestring {
         let arcs: Vec<Arc> = points
             .windows(2)
-            .map(|arc| Arc::new(&arc[0], &arc[1])._make_good_line())
-            .flatten()
+            .map(|arc| Arc::new(&arc[0], &arc[1]))
+            // .flatten()
             .collect();
         let mut points: Vec<Point> = arcs.iter().map(|arc| arc.from().clone()).collect();
         if !points.is_empty() {
@@ -23,12 +23,37 @@ impl Linestring {
     }
 
     pub fn to_feature(&self) -> Feature {
-        let point = Geometry::new(Value::LineString(
-            self.points.iter().map(|p| p.to_geojson_vec()).collect(),
-        ));
+        let mut points: Vec<_> = self.points.iter().map(|p| p.to_geojson_vec()).collect();
+        let mut factor = 0.0;
+
+        let mut factors = vec![0.0; points.len()];
+        factors[0] = 0.0;
+        if points.len() > 0 {
+            let mut i = 0;
+            while i + 1 < points.len() {
+                let pi = &points[i];
+                let pi1 = &points[i + 1];
+                if pi[0].abs() > 170.0 {
+                    if (pi[0].signum() == 1.0) && (pi1[0].signum() == -1.0) {
+                        factor += 360.0;
+                    } else if (pi[0].signum() == -1.0) && (pi1[0].signum() == 1.0) {
+                        factor -= 360.0;
+                    }
+                }
+
+                i += 1;
+                factors[i] = factor;
+            }
+        }
+
+        for i in 0..points.len() {
+            points[i][0] += factors[i];
+        }
+
+        let mut points = Geometry::new(Value::LineString(points));
         Feature {
             bbox: None,
-            geometry: Some(point),
+            geometry: Some(points),
             id: None,
             properties: None,
             foreign_members: None,
