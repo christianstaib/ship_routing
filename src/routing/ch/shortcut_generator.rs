641a6287d7bb400f876c600499fb365e3b195549
Christian Staib <staib.christian@hotmail.de>
2023-12-27 21:49:12 +0100

    did stuff

--- src/routing/ch/shortcut_generator.rs
+++ src/routing/ch/shortcut_generator.rs
@@ -19,32 +19,63 @@ impl<'a> ShortcutGenerator<'a> {
         let uv_edges = &self.graph.backward_edges[v as usize];
         let vw_edges = &self.graph.forward_edges[v as usize];
 
-        uv_edges
-            .iter()
-            .par_bridge()
-            .flat_map(|uv_edge| {
-                let mut shortcuts = Vec::new();
-                let u = uv_edge.source;
-                let uv_cost = uv_edge.cost;
+        if uv_edges.len() < 40 {
+            uv_edges
+                .iter()
+                .flat_map(|uv_edge| {
+                    let mut shortcuts = Vec::new();
+                    let u = uv_edge.source;
+                    let uv_cost = uv_edge.cost;
 
-                let max_cost = uv_cost + vw_edges.iter().map(|edge| edge.cost).max().unwrap_or(0);
+                    let max_cost =
+                        uv_cost + vw_edges.iter().map(|edge| edge.cost).max().unwrap_or(0);
 
-                let costs = dijkstra_helper.single_source_cost_without(u, v, max_cost);
-                vw_edges.iter().for_each(|vw_ede| {
-                    let w = vw_ede.target;
-                    let vw_cost = vw_ede.cost;
-                    let cost = uv_cost + vw_cost;
-                    if &cost < costs.get(&w).unwrap_or(&u32::MAX) {
-                        let shortcut = Edge {
-                            source: u,
-                            target: w,
-                            cost,
-                        };
-                        shortcuts.push((shortcut, vec![uv_edge.clone(), vw_ede.clone()]));
-                    }
-                });
-                shortcuts
-            })
-            .collect()
+                    let costs = dijkstra_helper.single_source_cost_without(u, v, max_cost);
+                    vw_edges.iter().for_each(|vw_ede| {
+                        let w = vw_ede.target;
+                        let vw_cost = vw_ede.cost;
+                        let cost = uv_cost + vw_cost;
+                        if &cost < costs.get(&w).unwrap_or(&u32::MAX) {
+                            let shortcut = Edge {
+                                source: u,
+                                target: w,
+                                cost,
+                            };
+                            shortcuts.push((shortcut, vec![uv_edge.clone(), vw_ede.clone()]));
+                        }
+                    });
+                    shortcuts
+                })
+                .collect()
+        } else {
+            uv_edges
+                .iter()
+                .par_bridge()
+                .flat_map(|uv_edge| {
+                    let mut shortcuts = Vec::new();
+                    let u = uv_edge.source;
+                    let uv_cost = uv_edge.cost;
+
+                    let max_cost =
+                        uv_cost + vw_edges.iter().map(|edge| edge.cost).max().unwrap_or(0);
+
+                    let costs = dijkstra_helper.single_source_cost_without(u, v, max_cost);
+                    vw_edges.iter().for_each(|vw_ede| {
+                        let w = vw_ede.target;
+                        let vw_cost = vw_ede.cost;
+                        let cost = uv_cost + vw_cost;
+                        if &cost < costs.get(&w).unwrap_or(&u32::MAX) {
+                            let shortcut = Edge {
+                                source: u,
+                                target: w,
+                                cost,
+                            };
+                            shortcuts.push((shortcut, vec![uv_edge.clone(), vw_ede.clone()]));
+                        }
+                    });
+                    shortcuts
+                })
+                .collect()
+        }
     }
 }
